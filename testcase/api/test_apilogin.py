891aad6eedc070b1e06f619a735936cdfa3261cb
jackyang1983 <sk.yang@163.com>
2022-04-30 15:48:01 +0800

    add logging configuration in pytest.ini

--- testcase/api/test_apilogin.py
+++ testcase/api/test_apilogin.py
@@ -14,17 +14,15 @@ class TestAppLogin:
     def test_baiduapi(self,api_fixture,args):
         s,base_url =  api_fixture
         result = s.get(base_url)
-        print()
         assert result.status_code == 200
-        # print(args['name'],args['age'])
 
-    @pytest.mark.smoke
-    @allure.severity('blocker')
-    def test_baiduui(self,ui_fixture):
-        driver,base_url =  ui_fixture
-        driver.get(base_url)
-        driver.find_element_by_id('kw').send_keys('iTesting')
-        driver.find_element_by_id('su').click()
-        search_result = driver.find_element_by_xpath('//*[@id="1"]/div/div[1]/h3/a').get_attribute('innerHTML')
-        print(search_result)
-        assert 'iTesting' in search_result
+    # @pytest.mark.smoke
+    # @allure.severity('blocker')
+    # def test_baiduui(self,ui_fixture):
+    #     driver,base_url =  ui_fixture
+    #     driver.get(base_url)
+    #     driver.find_element_by_id('kw').send_keys('iTesting')
+    #     driver.find_element_by_id('su').click()
+    #     search_result = driver.find_element_by_xpath('//*[@id="1"]/div/div[1]/h3/a').get_attribute('innerHTML')
+    #     print(search_result)
+    #     assert 'iTesting' in search_result
